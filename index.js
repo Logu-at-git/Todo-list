fed27eaa0b6f907c91709a12d8040f6c091312a6
Logu-at-git <142791846+Logu-at-git@users.noreply.github.com>
2023-08-24 10:38:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+import bodyParser from "body-parser";
+import express from "express";
+
+const app=express();
+const port=3000;
+const todo_list=[];
+const wish_list=[];
+const date=new Date().getDate();
+const month=new Date().getMonth();
+const year=new Date().getFullYear();
+const fulldate=date+"-"+month+"-"+year;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+app.listen(port,()=>{
+    
+    console.log(`Listening to port ${port}!`);
+    
+})
+
+app.post("/submitTodo",(req,res)=>{
+    const newItem=req.body["newListItem"];
+    todo_list.push(newItem);
+    res.render("index.ejs",{todo:todo_list,fulldate});
+    
+})
+
+app.post("/submitWish",(req,res)=>{
+    const newItem=req.body["newListItem"];
+    wish_list.push(newItem); 
+    res.render("wish.ejs",{wish:wish_list});
+})
+
+ 
+
+app.get("/",(req,res)=>{
+    
+    res.render("index.ejs",{todo:todo_list,fulldate});
+})
+
+app.get("/wish",(req,res)=>{
+    res.render("wish.ejs",{wish:wish_list});
+})
+
